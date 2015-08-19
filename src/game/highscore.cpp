e7377319dfb192cd451031636c1d3370f247f3eb
lowkey <mephistopheles@onlinehome.de>
2015-08-19 15:15:21 +0200

    online leaderboard for js-port

--- src/game/highscore.cpp
+++ src/game/highscore.cpp
@@ -2,10 +2,15 @@
 
 #include <core/asset/asset_manager.hpp>
 
+#include <core/utils/md5.hpp>
+
 #include <iomanip>
 #include <sf2/sf2.hpp>
 #include <sf2/FileParser.hpp>
 
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
 
 namespace mo {
 
@@ -22,6 +27,10 @@ namespace mo {
 		};
 
 		sf2_structDef(Score_list, sf2_member(scores))
+
+#ifdef EMSCRIPTEN
+		constexpr const char* base_url = "http://second-system.de/leaderboard.php";
+#endif
 	}
 
 	namespace asset {
@@ -45,6 +54,20 @@ namespace mo {
 
 
 	void add_score(asset::Asset_manager& assets, Score score) {
+
+#ifdef EMSCRIPTEN
+		std::stringstream cs_str;
+		cs_str<<"magnumOpus"<<score.name<<score.score<<score.level<<score.seed<<"42#23";
+
+		std::string cs = util::md5(cs_str.str());
+
+		std::stringstream url;
+		url<<base_url;
+		url<<"?game=magnumOpus&op=add&name="<<score.name<<"&score="<<score.score<<"&level="<<score.level<<"&seed="<<score.seed<<"&cs="<<cs;
+		emscripten_async_wget_data(url.str().c_str(), nullptr, +[](void* arg, void* data, int len){
+		}, +[](void*){});
+
+#else
 		auto stored_scores = assets.load_maybe<Score_list>("cfg:highscore"_aid);
 
 		auto score_list = stored_scores.process(std::vector<Score>{}, [](auto& ss){return ss->scores;});
@@ -58,12 +81,31 @@ namespace mo {
 		score_list.resize(std::min(score_list.size(), std::size_t(15)));
 
 		assets.save("cfg:highscore"_aid, Score_list{score_list});
+#endif
 	}
 
 	auto list_scores(asset::Asset_manager& assets) -> std::vector<Score> {
+#ifdef EMSCRIPTEN
+		Score_list scores;
+		scores.scores.reserve(15);
+
+		std::stringstream url;
+		url<<base_url<<"?game=magnumOpus&op=list";
+		emscripten_async_wget_data(url.str().c_str(), &scores, +[](void* arg, void* data, int len){
+			auto& target = *(Score_list*)(arg);
+
+			std::string resp((char*)data, len);
+
+			sf2::io::StringCharSource source(resp);
+			sf2::ParserDefChooser<Score_list>::get().parse(source, target);
+
+		}, +[](void*){});
+
+#else
 		auto stored_scores = assets.load_maybe<Score_list>("cfg:highscore"_aid);
 
 		return stored_scores.process(std::vector<Score>{}, [](auto& ss){return ss->scores;});
+#endif
 	}
 
 	auto print_scores(std::vector<Score> scores) -> std::string {
